805f74b8ae95b25913ddd5dd63ebd0dfc49a0dc5
jhc <coombsjh@gmail.com>
2015-01-27 16:20:53 -0500

    fake contacts

--- crm.rb
+++ crm.rb
@@ -1,15 +1,24 @@
 require 'sinatra'
+require_relative 'contact'
+
 
 get '/' do
-  @crm_app_name = "My CRM"
-  erb :index
+	  @crm_app_name = "My CRM"
+	  erb :index
 end
 
 get '/contacts' do
-  erb :contacts
+
+	  @contacts = []
+	  @contacts << Contact.new("Yehuda", "Katz", "yehuda@example.com", "Developer")
+	  @contacts << Contact.new("Mark", "Zuckerberg", "mark@facebook.com", "CEO")
+	  @contacts << Contact.new("Sergey", "Brin", "sergey@google.com", "Co-Founder")
+
+	erb :contacts
 end
 
+
 get '/contacts/new' do
-  erb :new
+	erb :new
 end
 
